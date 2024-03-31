9765f9546f54b335880e6bab0db9206b4e9c4720
asqarbekolimov <mr.coderx.pro@gmail.com>
2024-03-31 15:39:24 +0500

    feat: 3-masala

---
+++ Begin/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int P, a;
+
+    cout<<"a= ";cin>>a;
+    cout<< 4* a;
+    
+
+    return 0;
+}
---
+++ Begin/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  int S,a;
+
+  cout<<"a= "; cin>>a;
+  S=pow(a, 2);
+  
+  cout<<"S= "<<S<<endl;
+
+  return 0;
+}
